e8625ef41f26c007084ad1679a109f44c93811dd
jonathanekman <jonathanekman98@gmail.com>
2021-01-13 22:38:32 +0100

    border removed

--- speedo/main.js
+++ speedo/main.js
@@ -15,8 +15,8 @@ require('electron-reload')(__dirname + '/app/index.html', {
 
 
 function newApp() {
-  // win = new BrowserWindow({backgroundColor: "#112", width : 1024, height : 600, frame: false, x: 0, y: 0});       //<- removes border
-  win = new BrowserWindow({ backgroundColor: "#112", width : 1024, height : 600, x: 900, y: 0});
+  win = new BrowserWindow({backgroundColor: "#112", width : 1024, height : 600, frame: false, x: 0, y: 0});       //<- removes border
+  // win = new BrowserWindow({ backgroundColor: "#112", width : 1024, height : 600, x: 900, y: 0});
   win.removeMenu()
   win.loadFile('index.html');
   // win.webContents.openDevTools()  //development moode
