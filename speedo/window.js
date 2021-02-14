65120dabceebe71d1b3644a8648e2c21207f712e
jonathanekman <jonathanekman98@gmail.com>
2021-02-14 02:25:31 +0100

    readSpeed.py added

--- speedo/window.js
+++ speedo/window.js
@@ -2,16 +2,23 @@ let updatedSpeed
 
 window.setInterval(function(){    //catch input every 2 seconds
   update();
-}, 2000);
+}, 1000);
 
 function update(){
-  if ((document.getElementById("speed")) != null) {
-    var speed = document.getElementById('speed').value;  
-  }
-  
-  document.getElementById("kmh").innerHTML = speed + " km/h";
+//   if ((document.getElementById("speed")) != null) {
+//     var speed = document.getElementById('speed').value;  
+//   }
 
-  updatedSpeed = Math.round(speed*180/100)-45;
-  $("#speedbox-score").css("transform","rotate("+updatedSpeed+"deg)");  
+  $.getJSON("kph.json", function(json) {  //Reading Json
+    // console.log(json.speed);
+    var speed = json.speed;
+
+
+    document.getElementById("kmh").innerHTML = speed + " km/h";
+
+    updatedSpeed = Math.round(speed*180/100)-45;
+    $("#speedbox-score").css("transform","rotate("+updatedSpeed+"deg)");  
+});
+
 };
 
